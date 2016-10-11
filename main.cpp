e70bf144cbe3cb914342899c194b98434dad9886
Thomas Garcia <thomasjgarcia1@gmail.com>
2016-10-10 21:52:34 -0700

    Added support for serial.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ Motor right_motor(PB_10, PC_7);
 QEI left_encoder(PA_15, PB_3, NC, 12, QEI::X4_ENCODING);
 QEI right_encoder(PA_1, PC_4, NC, 12, QEI::X4_ENCODING);
 
-//Serial serial(USBTX, USBRX);
+Serial serial(PA_9, PA_10);
 
 int main()
 {
@@ -17,6 +17,6 @@ int main()
     while(right_encoder.getPulses() > -10 && right_encoder.getPulses() < 10) {
         left_motor.set_speed(0.1);
         right_motor.set_speed(0.1);
-        //serial.printf("Left: %d, Right: %d\n", left_encoder.getPulses(), right_encoder.getPulses());
+        serial.printf("Left: %d, Right: %d\n", left_encoder.getPulses(), right_encoder.getPulses());
     }
 }
