bb73bca2caecb579c9d1ec3fdd01cf4c8fe67a03
Thomas Garcia <thomasjgarcia1@gmail.com>
2016-11-05 16:29:26 -0700

    god tier P controller

--- motor.cpp
+++ motor.cpp
@@ -9,6 +9,12 @@ Motor::Motor(PinName forward, PinName backward, float multiplier)
 
 void Motor::set_speed(float speed)
 {
+    const float MAX_SPEED = 0.3;
+    if(speed > MAX_SPEED) {
+        speed = MAX_SPEED;
+    } else if(speed < -1 * MAX_SPEED) {
+        speed = -1 * MAX_SPEED;
+    }
     speed *= multiplier;
     if(speed >= 0) {
         backward.write(0);
