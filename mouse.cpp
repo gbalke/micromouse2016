68622be5b28b222448d29ae7ea5ea3fa69ab71b1
Thomas Garcia <thomasjgarcia1@gmail.com>
2017-05-19 14:33:32 -0700

    added saturation to PID, added distance sensor class, and added IR/encoder PID controllers to go straight

--- mouse.cpp
+++ mouse.cpp
@@ -5,6 +5,7 @@
 #include "gyro.h"
 #include "pid.h"
 #include "digital_output.h"
+#include "distance_sensor.h"
 
 #include "mbed.h"
 
@@ -20,8 +21,8 @@ HAL::Timer::Channel lbwd(motor_timer, HAL::Timer::CH1, HAL::Timer::Channel::COMP
 HAL::Timer::Channel lfwd(motor_timer, HAL::Timer::CH2, HAL::Timer::Channel::COMPARE_PWM1, PC_7);
 HAL::Timer::Channel rbwd(motor_timer, HAL::Timer::CH4, HAL::Timer::Channel::COMPARE_PWM1, PC_9);
 HAL::Timer::Channel rfwd(motor_timer, HAL::Timer::CH3, HAL::Timer::Channel::COMPARE_PWM1, PC_8);
-Motor left(lfwd, lbwd);
-Motor right(rfwd, rbwd);
+Motor left_motor(lfwd, lbwd);
+Motor right_motor(rfwd, rbwd);
 
 TimerEncoder left_encoder(HAL::Timer::TIMER2, PA_1, PA_0);
 InterruptEncoder right_encoder(PB_3, PA_15, InterruptEncoder::X4);
@@ -35,26 +36,25 @@ DigitalOutput red(PB_15);
 DigitalOutput green(PB_14);
 DigitalOutput blue(PB_13);
 
-IRSensor left_irsensor(PC_3, PB_6);
-IRSensor right_irsensor(PC_0, PB_8);
-IRSensor front_left_irsensor(PC_2, PB_7);
-IRSensor front_right_irsensor(PC_1, PB_9);
+DistanceSensor left_sensor(PC_3, PB_6, DistanceSensor::LOGISTIC, 1925.5, 0.4, 4073.8, 7.7);
+DistanceSensor right_sensor(PC_0, PB_8, DistanceSensor::LOGISTIC, 2298.3, 0.4, 4129.8, 6.5);
+DistanceSensor left_side_sensor(PC_2, PB_7, DistanceSensor::EXPONENTIAL, 9879.0, 0.5, 897.0, 0.0);
+DistanceSensor right_side_sensor(PC_1, PB_9, DistanceSensor::EXPONENTIAL, 7875.0, 0.4, 1324.6, 0.0);
 
 //Gyro gyro(PC_10, PC_12, PC_11, PA_4, 1e7);
 
-Pid controller(20, 0.03, 0);
+Pid encoder_controller(20, 0.03, 0);
+Pid ir_controller(30, 0, 0);
 
-float battery_level();
-
-float ir_distance_logistic(float reading, float scale, float time_constant,
-                            float midpoint, float max);
-float ir_distance_exp(float reading, float scale, float time_constant, float min);
+double battery_level();
+bool is_side_wall(double reading);
 
 int main()
 {
     motor_timer.set_period(511);
     motor_timer.enable(true);
     //gyro.calibrate();
+    bool ir_pid = true;
     while(true) {
         if(battery_level() < 7.4) {
             red.write(1);
@@ -65,7 +65,27 @@ int main()
             const int BASE_SPEED = 30;
             int left_speed = BASE_SPEED;
             int right_speed = BASE_SPEED;
-            int correction = (int)controller.correction(left_encoder.count() - right_encoder.count());
+            double left = left_sensor.read();
+            double right = right_sensor.read();
+            double left_side = left_side_sensor.read();
+            double right_side = right_side_sensor.read();
+            bool is_left_wall = is_side_wall(left_side);
+            bool is_right_wall = is_side_wall(right_side);
+            if(ir_pid && (!is_left_wall || !is_right_wall)) {
+                ir_pid = false;
+                left_encoder.reset();
+                right_encoder.reset();
+                encoder_controller.reset();
+            } else if(!ir_pid && is_left_wall && is_right_wall) {
+                ir_pid = true;
+                ir_controller.reset();
+            }
+            int correction;
+            if(ir_pid) {
+                correction = (int)ir_controller.correction(left_side - right_side);
+            } else {
+                correction = (int)encoder_controller.correction(left_encoder.count() - right_encoder.count());
+            }
             if(correction > 0) {
                 right_speed += correction;
             } else {
@@ -73,55 +93,22 @@ int main()
             }
             left_speed = ((unsigned) left_speed > 100) ? 100 : left_speed;
             right_speed = ((unsigned) right_speed > 100) ? 100 : right_speed;
-            left.set_speed(left_speed);
-            right.set_speed(right_speed);
+            left_motor.set_speed(left_speed);
+            right_motor.set_speed(right_speed);
         } else {
-            left.set_speed(0);
-            right.set_speed(0);
+            left_motor.set_speed(0);
+            right_motor.set_speed(0);
         }
-        int left = left_irsensor.read();
-        int right = right_irsensor.read();
-        //serial.printf("l %d\r\n", left_encoder.count());
-        //serial.printf("r %d\r\n", right_encoder.count());
-        //serial.printf("l %f\r\n", ir_distance(left, 1925.5, 0.4, 7.7, 4073.8));
-        //serial.printf("r %f\r\n", ir_distance(right, 2298.3, 0.4, 6.5, 4129.8));
-        serial.printf("fl %f\r\n", ir_distance_exp(front_left_irsensor.read(), 9879, 0.5, 897));
-        serial.printf("fr %f\r\n", ir_distance_exp(front_right_irsensor.read(), 7875, 0.4, 1324.6));
-        wait(1);
     }
 }
 
-float battery_level()
+double battery_level()
 {
     AnalogIn voltage_divider(PC_5);
     return (8.3 / 54700) * voltage_divider.read_u16();
 }
 
-static float max_distance = 0;
-
-float ir_distance_exp(float reading, float scale, float time_constant, float min)
+bool is_side_wall(double reading)
 {
-    float dist = -1 * log((reading - min) / scale) / time_constant;
-    if(isnan(dist)) {
-        return max_distance;
-    } else {
-        if(dist > max_distance) {
-            max_distance = dist;
-        }
-    }
-    return dist;
-}
-
-float ir_distance_logistic(float reading, float scale, float time_constant,
-                            float midpoint, float max)
-{
-    float dist = midpoint - log(scale / (max - reading) - 1) / time_constant;
-    if(isnan(dist)) {
-        return max_distance;
-    } else {
-        if(dist > max_distance) {
-            max_distance = dist;
-        }
-    }
-    return dist;
+    return !isnan(reading) && reading < 8;
 }
